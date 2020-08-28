621011b9ec4adfe0d4fd6190503e3c940bde5fd5
Haidy  Farahat <haidyfarahat@gmail.com>
2020-08-28 04:52:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -74,7 +74,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash [:yukihiro_matsumoto] = ":known_for => "Ruby", :languages => ["LISP", "C"]"
+  programmer_hash [:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}]
   return programmer_hash
 end
 
@@ -97,7 +97,6 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      programmer_hash[:alan_kay][:known_for]= "GUI"
 return programmer_hash
-
 end
 
 def adding_to_dennis
